402454173bb54dd0995b854110bbef53194f213a
artiumex <moses@thevinsons.com>
2023-12-01 12:44:24 -0600

    oopsie

---
+++ src/config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+module.exports = {
+    client: {
+        token: process.env.DISCORD_TOKEN,
+        id: process.env.DISCORD_BOTID,
+    },
+    handler: {
+        prefix: "Dabloon, ",
+        deploy: true,
+        commands: {
+            prefix: false,
+            slash: true,
+            user: true,
+            message: true,
+        },
+        mongodb: {
+            uri: process.env.MONGO_URI,
+            toggle: false,
+        },
+    },
+    users: {
+        developers: [process.env.ADMIN],
+    },
+    messageSettings: {
+        nsfwMessage: "The current channel is not a NSFW channel.",
+        developerMessage: "You are not authorized to use this command.",
+        cooldownMessage: "Slow down buddy!",
+        notHasPermissionMessage: "You do not have the permission to use this command.",
+        missingDevIDsMessage: "This is a developer only command, but unable to execute due to missing user IDs in configuration file."
+    }
+};
