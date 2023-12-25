e3386311b4a431e7477934ba105883476809cdee
artiumex <moses@thevinsons.com>
2023-12-25 12:48:17 -0600

    yasssss north daddy

--- src/jobs/demons.js
+++ src/jobs/demons.js
@@ -1,4 +1,5 @@
 const ExtendedClient = require('../class/ExtendedClient');
+const Bot = require('../schemas/BotSettingsSchema');
 
 module.exports = {
     name: 'demons',
@@ -10,16 +11,18 @@ module.exports = {
      */
     run: async (client) => {
         const gods = {
-            26: { name: "Osiris", file: "osiris.jpg" },
-            27: { name: "Horus", file: "horus.jpg" },
-            28: { name: "Set", file: "set.jpg" },
-            29: { name: "Isis", file: "isis.jpg" },
-            30: { name: "Nepthys", file: "nepthys.jpg" },
+            25: { name: "Klaus", file: "north.jpg", status: "Christmas DILF" },
+            26: { name: "Osiris", file: "osiris.jpg", status: "I'm literally god of the dead AND the living" },
+            27: { name: "Horus", file: "horus.jpg", status: "will smite a mf" },
+            28: { name: "Set", file: "set.jpg", status: "not evil, just complex" },
+            29: { name: "The Lady, Isis", file: "isis.jpg", status: "its just magic you guys" },
+            30: { name: "Nepthys", file: "nepthys.jpg", status: "denial is a river in egypt" },
         }
         const now = new Date();
         const god = gods[now.getDate()];
         await client.user.setAvatar('./src/data/avatars/ddays/'+god.file).catch(client.nerrify);
         await client.allNicknames(god.name, "Demon Days Event").catch(client.nerrify);
+        await Bot.updateOne({ botid: client.user.id }, { current_status: god.status }).catch(client.nerrify);
         await client.notify(god.name + ' Mode Active!', 'event');
     }
 }
